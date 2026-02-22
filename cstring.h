1b3dbb0dccc0ac7e57027a690c93b751415a517d
Aleksa <aleksa.milosevic807@gmail.com>
2026-02-23 00:13:20 +0100

    Implement cstr_cmp_str and cstr_cmp_cstr functions for string comparison

--- cstring.h
+++ cstring.h
@@ -8,44 +8,44 @@
 typedef struct {
     uint64_t length;
     char data[];
-} cstr;
+} cstr_t;
 
-static cstr* cstr_alloc(const char* str);
-static void cstr_free(cstr** cstr);
+static cstr_t* cstr_alloc(const char* str);
+static void cstr_free(cstr_t** cstr);
 static uint64_t cstr_len(const char* str);
 static void cstr_copy(void* dst, const void* src, uint64_t length);
-static void cstr_concat_str(cstr** cstr_dst, const char* str_src);
-static void cstr_concat_cstr(cstr** cstr_dst, const cstr* cstr_src);
-static int cstr_cmp_str(const cstr* cstr, const char* str);
-static int cstr_cmp_cstr(const cstr* cstr_1, const cstr* cstr_2);
+static void cstr_concat_str(cstr_t** cstr_dst, const char* str_src);
+static void cstr_concat_cstr(cstr_t** cstr_dst, const cstr_t* cstr_src);
+static int32_t cstr_cmp_str(const cstr_t* cstr, const char* str);
+static int32_t cstr_cmp_cstr(const cstr_t* cstr_1, const cstr_t* cstr_2);
 
 #ifdef CSTR_IMPLEMENTATION
 
-cstr* cstr_alloc(const char* str) {
-    uint64_t length;
+cstr_t* cstr_alloc(const char* str) {
+    uint64_t str_length;
     uint64_t cstr_size;
-    cstr* cstring = NULL;
+    cstr_t* cstr = NULL;
 
-    length = cstr_len(str);
-    cstr_size = length + 1 + sizeof(cstr);
-    cstring = (cstr*)malloc(cstr_size);
-    assert(cstring != NULL);
+    str_length = cstr_len(str);
+    cstr_size = str_length + 1 + sizeof(cstr_t);
+    cstr = (cstr_t*)malloc(cstr_size);
+    assert(cstr != NULL);
 
-    cstring->length = length;
-    cstr_copy(cstring->data, str, length);
-    return cstring;
+    cstr->length = str_length;
+    cstr_copy(cstr->data, str, str_length);
+    return cstr;
 }
 
-void cstr_free(cstr** cstr) {
+void cstr_free(cstr_t** cstr) {
     free(*cstr);
     *cstr = NULL;
 }
 
 uint64_t cstr_len(const char* str) {
-    uint64_t length = 0;
-    const char* ptr = str;
-    for (; *ptr != 0; ptr++, length++);
-    return length;
+    uint64_t str_length = 0;
+    const char* str_ptr = str;
+    for (; *str_ptr != 0; str_ptr++, str_length++);
+    return str_length;
 }
 
 void cstr_copy(void* dst, const void* src, uint64_t length) {
@@ -54,23 +54,38 @@ void cstr_copy(void* dst, const void* src, uint64_t length) {
     for (; length > 0; ptr_dst++, ptr_src++, length--) *ptr_dst = *ptr_src;
 }
 
-void cstr_concat_str(cstr** cstr_dst, const char* str_src) {
+void cstr_concat_str(cstr_t** cstr_dst, const char* str_src) {
     uint64_t str_length = cstr_len(str_src);
-    uint64_t cstr_curr_size = (*cstr_dst)->length + 1 + sizeof(cstr);
+    uint64_t cstr_curr_size = (*cstr_dst)->length + 1 + sizeof(cstr_t);
     uint64_t cstr_new_size = cstr_curr_size + str_length;
 
-    *cstr_dst = (cstr*)realloc(*cstr_dst, cstr_new_size);
+    *cstr_dst = (cstr_t*)realloc(*cstr_dst, cstr_new_size);
     char* cstr_data = (*cstr_dst)->data + (*cstr_dst)->length;
     cstr_copy(cstr_data, str_src, str_length);
     (*cstr_dst)->length += str_length;
 }
 
-void cstr_concat_cstr(cstr** cstr_dst, const cstr* cstr_src) {
+void cstr_concat_cstr(cstr_t** cstr_dst, const cstr_t* cstr_src) {
     cstr_concat_str(cstr_dst, cstr_src->data);
 }
 
-int cstr_cmp_str(const cstr* cstr, const char* str) { return 0; }
-int cstr_cmp_cstr(const cstr* cstr_1, const cstr* cstr_2) { return 0; }
+int32_t cstr_cmp_str(const cstr_t* cstr, const char* str) {
+    uint64_t i = 0;
+    uint64_t str_length = cstr_len(str);
+    while (i < cstr->length && i < str_length) {
+        if (cstr->data[i] != str[i]) {
+            return (uint8_t)cstr->data[i] - (uint8_t)str[i];
+        }
+        i++;
+    }
+    if (cstr->length < str_length) return -1;
+    if (cstr->length > str_length) return 1;
+    return 0;
+}
+
+int32_t cstr_cmp_cstr(const cstr_t* cstr_1, const cstr_t* cstr_2) {
+    return cstr_cmp_str(cstr_1, cstr_2->data);
+}
 
 #endif // CSTR_IMPLEMENTATION
 #endif // CSTR_H
--- test.c
+++ test.c
@@ -3,17 +3,19 @@
 
 #include <stdio.h>
 int main() {
-    cstr* str = NULL;
-    str = cstr_alloc("Hello ");
-    cstr_concat_str(&str, "THERE");
-    printf("%s\n", str->data);
-    cstr* str1 = cstr_alloc(" THERE TWO!!!");
-    cstr_concat_cstr(&str, str1);
-    printf("%s\n", str->data);
-    cstr* str2 = cstr_alloc(" There three?!!");
-    cstr_concat_cstr(&str, str2);
-    printf("%s\n", str->data);
-    cstr_free(&str2);
+    cstr_t* str = NULL;
+    str = cstr_alloc("Helloa");
+    cstr_t* str1 = cstr_alloc("Hello");
+    printf("%d\n", cstr_cmp_cstr(str, str1));
+    // cstr_concat_str(&str, "THERE");
+    // printf("%s\n", str->data);
+    // cstr_t* str1 = cstr_alloc(" THERE TWO!!!");
+    // cstr_concat_cstr(&str, str1);
+    // printf("%s\n", str->data);
+    // cstr_t* str2 = cstr_alloc(" There three?!!");
+    // cstr_concat_cstr(&str, str2);
+    // printf("%s\n", str->data);
+    // cstr_free(&str2);
     cstr_free(&str1);
     cstr_free(&str);
     return 0;
