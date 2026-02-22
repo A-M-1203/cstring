28b2164201bf2e4ee7e66bc8edb97594d011d72b
Aleksa <aleksa.milosevic807@gmail.com>
2026-02-22 23:40:01 +0100

    Fix bug with cstr_concat_cstr function

--- cstring.h
+++ cstring.h
@@ -1,44 +1,44 @@
-#ifndef CSTRING_H
-#define CSTRING_H
+#ifndef CSTR_H
+#define CSTR_H
 
 #include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-typedef struct cstr_string {
+typedef struct {
     uint64_t length;
     char data[];
-} String;
+} cstr;
 
-static String* cstr_alloc(const char* str);
-static void cstr_free(String* cstr);
+static cstr* cstr_alloc(const char* str);
+static void cstr_free(cstr** cstr);
 static uint64_t cstr_len(const char* str);
 static void cstr_copy(void* dst, const void* src, uint64_t length);
-static void cstr_concat_str(String* cstr_dst, const char* str_src);
-static void cstr_concat_cstr(String* cstr_dst, const String* cstr_src);
-static int cstr_cmp_str(String* cstr, const char* str);
-static int cstr_cmp_cstr(String* cstr_1, String* cstr_2);
+static void cstr_concat_str(cstr** cstr_dst, const char* str_src);
+static void cstr_concat_cstr(cstr** cstr_dst, const cstr* cstr_src);
+static int cstr_cmp_str(const cstr* cstr, const char* str);
+static int cstr_cmp_cstr(const cstr* cstr_1, const cstr* cstr_2);
 
-#ifdef CSTRING_IMPLEMENTATION
+#ifdef CSTR_IMPLEMENTATION
 
-String* cstr_alloc(const char* str) {
+cstr* cstr_alloc(const char* str) {
     uint64_t length;
     uint64_t cstr_size;
-    String* cstr = NULL;
+    cstr* cstring = NULL;
 
     length = cstr_len(str);
-    cstr_size = length + 1 + sizeof(String);
-    cstr = (String*)malloc(cstr_size);
-    assert(cstr != NULL);
+    cstr_size = length + 1 + sizeof(cstr);
+    cstring = (cstr*)malloc(cstr_size);
+    assert(cstring != NULL);
 
-    cstr->length = length;
-    cstr_copy(cstr->data, str, length);
-    return cstr;
+    cstring->length = length;
+    cstr_copy(cstring->data, str, length);
+    return cstring;
 }
 
-void cstr_free(String* cstr) {
-    free(cstr);
-    cstr = NULL;
+void cstr_free(cstr** cstr) {
+    free(*cstr);
+    *cstr = NULL;
 }
 
 uint64_t cstr_len(const char* str) {
@@ -54,23 +54,23 @@ void cstr_copy(void* dst, const void* src, uint64_t length) {
     for (; length > 0; ptr_dst++, ptr_src++, length--) *ptr_dst = *ptr_src;
 }
 
-void cstr_concat_str(String* cstr_dst, const char* str_src) {
+void cstr_concat_str(cstr** cstr_dst, const char* str_src) {
     uint64_t str_length = cstr_len(str_src);
-    uint64_t cstr_curr_size = cstr_dst->length + 1 + sizeof(String);
+    uint64_t cstr_curr_size = (*cstr_dst)->length + 1 + sizeof(cstr);
     uint64_t cstr_new_size = cstr_curr_size + str_length;
 
-    cstr_dst = (String*)realloc(cstr_dst, cstr_new_size);
-    char* cstr_data = cstr_dst->data + cstr_dst->length;
+    *cstr_dst = (cstr*)realloc(*cstr_dst, cstr_new_size);
+    char* cstr_data = (*cstr_dst)->data + (*cstr_dst)->length;
     cstr_copy(cstr_data, str_src, str_length);
-    cstr_dst->length += str_length;
+    (*cstr_dst)->length += str_length;
 }
 
-void cstr_concat_cstr(String* cstr_dst, const String* cstr_src) {
+void cstr_concat_cstr(cstr** cstr_dst, const cstr* cstr_src) {
     cstr_concat_str(cstr_dst, cstr_src->data);
 }
 
-int cstr_cmp_str(String* cstr, const char* str) { return 0; }
-int cstr_cmp_cstr(String* cstr_1, String* cstr_2) { return 0; }
+int cstr_cmp_str(const cstr* cstr, const char* str) { return 0; }
+int cstr_cmp_cstr(const cstr* cstr_1, const cstr* cstr_2) { return 0; }
 
-#endif // CSTRING_IMPLEMENTATION
-#endif // CSTRING_H
+#endif // CSTR_IMPLEMENTATION
+#endif // CSTR_H
